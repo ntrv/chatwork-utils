73b4724acedc0942b26b34f0248cf571488b9567
Nate-River56 <hygrangea56@me.com>
2017-04-16 01:37:53 +0900

    Append testing environment

---
+++ test/sample.js
@@ -0,0 +1,10 @@
+import assert from 'assert';
+
+describe('ES6 test of test in chatwork', () => {
+  it('Sample: Add calculation', () => {
+    const expect = 2;
+    const actual = 1 + 1;
+
+    assert(expect === actual);
+  });
+});

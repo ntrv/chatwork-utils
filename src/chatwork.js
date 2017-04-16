d3069da08a576d25f78a39689326de399220c8e4
Nate-River56 <hygrangea56@me.com>
2017-04-16 14:07:14 +0900

    Append testing code for options

--- src/chatwork.js
+++ src/chatwork.js
@@ -1,7 +1,4 @@
 import baseChatwork from './baseChatwork';
 
 export default class chatwork extends baseChatwork {
-  // constructor(apikey){
-  //   super(apikey);
-  // }
 }
--- test/options.js
+++ test/options.js
@@ -1,15 +1,29 @@
-import chatwork from '../index';
-import _ from 'lodash';
 import chai from 'chai';
+import assert from 'assert';
 
-const assert = chai.assert;
+import chatwork from '../index';
+
+const cassert = chai.assert;
 
 describe('Optionのテスト', () => {
   it('paramsが空のときformが存在しない', () => {
     const Cw = new chatwork('hoge');
     const actual = Cw.option('/me', 'GET');
-    
-    assert.isUndefined(actual.form);
+
+    cassert.isUndefined(actual.form);
   });
 
+  it('Optionの値が想定した通りに生成されているか', () => {
+    const Cw = new chatwork('fuga');
+    const actual = Cw.option('/room/123', 'POST', { body: 'foo' });
+    const expected = {
+      url: 'https://api.chatwork.com/v2/room/123',
+      method: 'POST',
+      headers: { 'X-ChatWorkToken': 'fuga' },
+      json: true,
+      form: { body: 'foo' },
+    };
+
+    assert(JSON.stringify(actual) === JSON.stringify(expected));
+  });
 });

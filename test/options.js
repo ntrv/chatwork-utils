d5ff4a7fa00c15f8f6d252c24080d8c45c8bdcdf
Nate-River56 <hygrangea56@me.com>
2017-04-16 14:24:12 +0900

    Make it available to lint testing code

--- test/options.js
+++ test/options.js
@@ -1,21 +1,21 @@
 import chai from 'chai';
 import assert from 'assert';
 
-import chatwork from '../index';
+import Chatwork from '../index';
 
 const cassert = chai.assert;
 
 describe('Optionのテスト', () => {
   it('paramsが空のときformが存在しない', () => {
-    const Cw = new chatwork('hoge');
-    const actual = Cw.option('/me', 'GET');
+    const cw = new Chatwork('hoge');
+    const actual = cw.option('/me', 'GET');
 
     cassert.isUndefined(actual.form);
   });
 
   it('Optionの値が想定した通りに生成されているか', () => {
-    const Cw = new chatwork('fuga');
-    const actual = Cw.option('/room/123', 'POST', { body: 'foo' });
+    const cw = new Chatwork('fuga');
+    const actual = cw.option('/room/123', 'POST', { body: 'foo' });
     const expected = {
       url: 'https://api.chatwork.com/v2/room/123',
       method: 'POST',

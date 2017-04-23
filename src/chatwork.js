8c53abe9c8bf1236ee2866ad82d25f3b489044cf
Nate-River56 <hygrangea56@me.com>
2017-04-24 00:51:42 +0900

    Append GET /rooms APIs

--- src/chatwork.js
+++ src/chatwork.js
@@ -27,4 +27,58 @@ export default class chatwork extends baseChatwork {
     return this.instance.get('/incoming_requests');
   }
 
+  listMyMessages() {
+    return this.instance.get('/rooms');
+  }
+
+  getRoomInfo(roomId) {
+    return this.instance.get(`/rooms/${roomId}`);
+  }
+
+  listRoomMembers(roomId) {
+    return this.instance.get(`/rooms/${roomId}/members`);
+  }
+
+  listRoomMessages(roomId, isForce = 0) {
+    return this.instance.get(`/rooms/${roomId}/messages`, {
+      params: {
+        force: isForce,
+      },
+    });
+  }
+
+  getMessageInfo(roomId, messageId) {
+    return this.instance.get(`/rooms/${roomId}/messages/${messageId}`);
+  }
+
+  listRoomTasks(roomId, accountId, assignedByAccountId, status = 'open') {
+    return this.instance.get(`/rooms/${roomId}/tasks`, {
+      params: {
+        account_id: accountId,
+        assigned_by_account_id: assignedByAccountId,
+        status,
+      },
+    });
+  }
+
+  getTaskInfo(roomId, taskId) {
+    return this.instance.get(`/rooms/${roomId}/tasks/${taskId}`);
+  }
+
+  listRoomFiles(roomId, accountId) {
+    return this.instance.get(`/rooms/${roomId}/files`, {
+      params: {
+        account_id: accountId,
+      },
+    });
+  }
+
+  getFileInfo(roomId, fileId, isUrl = 0) {
+    return this.instance.get(`/rooms/${roomId}/files/${fileId}`, {
+      params: {
+        create_download_url: isUrl,
+      },
+    });
+  }
+
 }

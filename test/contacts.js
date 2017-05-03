ac2ad70402d0d6bcb34adb92ab706cbbbbc36315
Nate-River56 <hygrangea56@me.com>
2017-05-04 01:36:53 +0900

    Modify expression of mocha in contacts.js

--- test/contacts.js
+++ test/contacts.js
@@ -6,32 +6,34 @@ import Chatwork from '../index';
 const chai = Chai.assert;
 
 describe('/contactsのテスト', () => {
-  it('GET /contacts', () => {
-    const cw = new Chatwork('apiKey');
-    const mock = new MockAdapter(cw.instance);
+  describe('GET', () => {
+    it('/contacts', () => {
+      const cw = new Chatwork('apiKey');
+      const mock = new MockAdapter(cw.instance);
 
-    const mockRes = [
-      {
-        account_id: 123,
-        room_id: 322,
-        name: 'John Smith',
-        chatwork_id: 'tarochatworkid',
-        organization_id: 101,
-        organization_name: 'Hello Company',
-        department: 'Marketing',
-        avatar_image_url: 'https://example.com/abc.png',
-      },
-    ];
+      const mockRes = [
+        {
+          account_id: 123,
+          room_id: 322,
+          name: 'John Smith',
+          chatwork_id: 'tarochatworkid',
+          organization_id: 101,
+          organization_name: 'Hello Company',
+          department: 'Marketing',
+          avatar_image_url: 'https://example.com/abc.png',
+        },
+      ];
 
-    mock.onGet('/contacts').reply(200, mockRes);
+      mock.onGet('/contacts').reply(200, mockRes);
 
-    return cw.listMyContacts().then(res => {
-      assert(
-        JSON.stringify(res.data) === JSON.stringify(mockRes),
-      );
-    })
-    .catch(err => {
-      chai.fail(0, 1, err.message);
+      return cw.listMyContacts().then(res => {
+        assert(
+          JSON.stringify(res.data) === JSON.stringify(mockRes),
+        );
+      })
+      .catch(err => {
+        chai.fail(0, 1, err.message);
+      });
     });
   });
 });

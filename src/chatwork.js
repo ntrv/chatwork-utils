5bdd069181f0d2cfc7eee1750b9cb6dbde3f3dff
Nate-River56 <hygrangea56@me.com>
2017-05-09 09:08:39 +0900

    Append incoming_requests update method

--- src/chatwork.js
+++ src/chatwork.js
@@ -5,4 +5,22 @@ import chatworkRo from './chatworkRo';
  * @desc For use Chatwork services
  */
 export default class chatwork extends chatworkRo {
+
+  /**
+   * @return {Promise} Return response or error message.
+   * @desc 自分に対するコンタクト承認依頼を承認する
+   * @param {number} requestId - リクエストID
+   */
+  approveIncomingRequest(requestId) {
+    return this.instance.put(`/incoming_requests/${requestId}`);
+  }
+
+  /**
+   * @return {Promise} Return response or error message.
+   * @desc 自分に対するコンタクト承認依頼をキャンセルする
+   * @param {number} requestId - リクエストID
+   */
+  rejectIncomingRequest(requestId) {
+    return this.instance.delete(`/incoming_requests/${requestId}`);
+  }
 }
--- test/incoming_requests.js
+++ test/incoming_requests.js
@@ -38,4 +38,58 @@ describe('/incoming_requestsのテスト', () => {
       });
     });
   });
+
+  describe('PUT', () => {
+    it('/incoming_requests/{{request_id}}', () => {
+      const cw = new Chatwork('apiKey');
+      const mock = new MockAdapter(cw.instance);
+      const requestId = 12;
+
+      const mockRes = {
+        account_id: 363,
+        room_id: 1234,
+        name: 'John Smith',
+        chatwork_id: 'tarochatworkid',
+        organization_id: 101,
+        organization_name: 'Hello Company',
+        department: 'Marketing',
+        avatar_image_url: 'https://example.com/abc.png',
+      };
+
+      mock.onPut(`/incoming_requests/${requestId}`)
+        .reply(200, mockRes);
+
+      return cw.approveIncomingRequest(requestId)
+      .then(res => {
+        assert(
+          JSON.stringify(res.data) === JSON.stringify(mockRes),
+        );
+      })
+      .catch(err => {
+        chai.fail(0, 1, err.message);
+      });
+    });
+  });
+
+  describe('DELETE', () => {
+    it('/incoming_requests/{{request_id}}', () => {
+      const cw = new Chatwork('apiKey');
+      const mock = new MockAdapter(cw.instance);
+      const requestId = 12;
+      const expectedCode = 200;
+
+      mock.onDelete(`/incoming_requests/${requestId}`)
+        .reply(expectedCode, {});
+
+      return cw.rejectIncomingRequest(requestId)
+      .then(res => {
+        assert(
+          res.status === expectedCode,
+        );
+      })
+      .catch(err => {
+        chai.fail(0, 1, err.message);
+      });
+    });
+  });
 });

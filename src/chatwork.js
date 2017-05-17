ea104e066970817de6a72d52eb10e61080e1d720
Nate-River56 <hygrangea56@me.com>
2017-05-18 03:20:53 +0900

    Create createChatroom and test

--- src/chatwork.js
+++ src/chatwork.js
@@ -1,4 +1,7 @@
+import qs from 'qs';
+import _ from 'lodash';
 import chatworkRo from './chatworkRo';
+import Utils from './utils';
 
 /**
  * @access public
@@ -23,4 +26,32 @@ export default class chatwork extends chatworkRo {
   rejectIncomingRequest(requestId) {
     return this.instance.delete(`/incoming_requests/${requestId}`);
   }
+
+  /**
+   * @return {Promise} Return response or error message.
+   * @desc グループチャットを新規作成
+   * @param {number} requestId - リクエストID
+   */
+  createChatroom(options) {
+    if (!Utils.isAllArrays([
+      options.membersAdminIds,
+      options.membersMemberIds,
+      options.membersReadonlyIds,
+    ])) {
+      return Promise.reject({ message: 'Fail' });
+    }
+
+    return this.instance.post('/rooms',
+      qs.stringify({
+        description: options.description,
+        members_admin_ids: _.join(options.membersAdminIds),
+        members_member_ids: _.join(options.membersMemberIds),
+        members_readonly_ids: _.join(options.membersReadonlyIds),
+        icon_preset: options.iconPreset,
+        name: options.name,
+      }),
+    );
+  }
+
+
 }
